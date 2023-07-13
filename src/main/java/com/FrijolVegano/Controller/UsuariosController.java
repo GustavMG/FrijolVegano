c1232d95aa777ea3868c11f525d1da400c117004
GustavMG <magg950506@gmail.com>
2023-07-13 14:12:58 -0600

    Se modifica el PutMappgin por PostMapping

--- src/main/java/com/FrijolVegano/Controller/UsuariosController.java
+++ src/main/java/com/FrijolVegano/Controller/UsuariosController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -44,7 +45,7 @@ public class UsuariosController {
 		
 	//POST
 	//Crear usuarios
-	@PutMapping
+	@PostMapping
 	public Usuarios postUsuarios(@RequestBody Usuarios usuarios) {
 		return usuariosService.crearUsuarios(usuarios);
 	}//postUsuarios
