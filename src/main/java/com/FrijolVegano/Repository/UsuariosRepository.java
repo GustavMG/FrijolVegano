415c7f0dc98797c9974b374714d73ff1a5c6eba5
GustavMG <magg950506@gmail.com>
2023-07-13 03:00:28 -0600

    Se agrega consulta personalizada 'buscarPorEmail'

--- src/main/java/com/FrijolVegano/Repository/UsuariosRepository.java
+++ src/main/java/com/FrijolVegano/Repository/UsuariosRepository.java
@@ -1,6 +1,6 @@
 package com.FrijolVegano.Repository;
 
-//import java.util.Optional;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -8,6 +8,6 @@ import org.springframework.stereotype.Repository;
 import com.FrijolVegano.Entity.Usuarios;
 @Repository
 public interface UsuariosRepository extends JpaRepository<Usuarios, Long> {
-//	Optional<Usuarios> buscarPorCorreo(String correo);
+	Optional<Usuarios> buscarPorEmail(String email);
 	//Aqui el correo es el dato importante ya que es con el que se identifican los usuarios
 }
