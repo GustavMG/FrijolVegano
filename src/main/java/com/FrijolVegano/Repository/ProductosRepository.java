5665cf56e7db372446f2a412bbfb4191e4485385
Eliseo de Haro <elidharolpz@gmail.com>
2023-07-13 11:07:18 -0600

    Se anexa el cambio en git repository

--- src/main/java/com/FrijolVegano/Repository/ProductosRepository.java
+++ src/main/java/com/FrijolVegano/Repository/ProductosRepository.java
@@ -10,5 +10,6 @@ import com.FrijolVegano.Entity.Productos;
 public interface ProductosRepository extends JpaRepository<Productos, Long> {
 //	Optional<Productos> buscarPorCodigoProducto(String codigoProducto);
 //	Lo mismo que para los cursos, un código interno de curso/producto pq seria dificil buscarlo con nombre
-	Optional<Productos> buscarPorNombre(String nombre);
+//	Optional<Productos> buscarPorNombre(String nombre);
+
 }
