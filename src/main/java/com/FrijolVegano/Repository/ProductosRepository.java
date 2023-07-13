2775134d06fe8f8af251b70b26faaa5f7f6e4b93
GustavMG <magg950506@gmail.com>
2023-07-12 23:55:42 -0600

    Se modifica ProductosRepository y ProductosService

--- src/main/java/com/FrijolVegano/Repository/ProductosRepository.java
+++ src/main/java/com/FrijolVegano/Repository/ProductosRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.FrijolVegano.Entity.Productos;
 
 public interface ProductosRepository extends JpaRepository<Productos, Long> {
-	Optional<Productos> buscarPorCodigoProducto(String codigoProducto);
+//	Optional<Productos> buscarPorCodigoProducto(String codigoProducto);
 	//Lo mismo que para los cursos, un código interno de curso/producto pq seria dificil buscarlo con nombre
+	Optional<Productos> buscarPorNombre(String nombre);
 }
--- src/main/java/com/FrijolVegano/Service/ProductosService.java
+++ src/main/java/com/FrijolVegano/Service/ProductosService.java
@@ -1,6 +1,7 @@
 package com.FrijolVegano.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -12,11 +13,50 @@ public class ProductosService {
 	@Autowired
 	public ProductosService(ProductosRepository productosRepository) {
 		this.productosRepository = productosRepository;
-	}
+	}//Constructor
 	
-	//Los metodos para los productos, deberian ser agregar, modificar y eliminar
+	//Los metodos para los PRODUCTOS, deberian ser agregar, modificar y eliminar
 	public List<Productos> leerProductos(){
 		return productosRepository.findAll();
-	}
-
+	}//leerProductos
+	
+	//Buscar/Leer PRODUCTO
+	public Optional<Productos> leerProductosId(Long id){
+		return productosRepository.findById(id);
+	}//leerProductos id
+	
+	//CREAR PRODUCTO
+	public Productos crearProductos(Productos productos) {
+		Productos productoTemp = null;
+//		if(productosRepository.buscarPorNombre(getNombre()).isEmpty())
+			productoTemp = productosRepository.save(productos);
+//		}
+		return productoTemp;
+	}//crearProductos
+	
+	//MODIFICAR PRODUCTO
+	public Productos modificarProductos(Productos productos) {
+		Productos productoTemp = null;
+//		if(productosRepository.existsById(id)) {
+		if(productosRepository.existsById(null)) {
+//			productoTemp = productosRepository.findById(id).get();
+			productoTemp = productosRepository.findById(null).get();
+			//TODO:Agregar las validaciones de las entradas, se requiere los parametros de la tabla, por ejemplo:
+//			if(nombre != null) productoTemp.setNombre(nombre);
+			
+			productoTemp = productosRepository.save(productos);
+		}
+		return productoTemp;
+	}//modificarProductos
+	
+	//ELIMINAR PRODUCTO
+	public Productos eliminarProductos(Productos productos) {
+		Productos productoTemp = null;
+//		if(productosRepository.existsById(id)) {
+		if(productosRepository.existsById(null)) {
+			productoTemp = productosRepository.findById(null).get();
+			productosRepository.delete(null);
+		}
+		return productoTemp;
+	}//eliminarProductos
 }
