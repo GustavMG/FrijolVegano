4d8e58466631af00fdddb8d0571fc8e6316f3601
Eliseo de Haro <elidharolpz@gmail.com>
2023-07-13 02:56:31 -0600

    Se anexa la entidad productos con todos sus atributos, constructor, setters y getters

--- src/main/java/com/FrijolVegano/Entity/Productos.java
+++ src/main/java/com/FrijolVegano/Entity/Productos.java
@@ -15,10 +15,192 @@ import javax.persistence.Table;
 @Entity
 @Table (name = "producto")
 public class Productos {
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	
+	@Id //Llave Primaria
+	@GeneratedValue(strategy = GenerationType.IDENTITY) //Comportamientos de la PK Autoincremental
+	
+	//Atributos ----------------------------------------------------------------------------------------------
+	
 	@Column(name = "id", unique = true, nullable = false)
 	private Long id;
+	private boolean esProducto;
+	private String nombre;
+	private String marca;
+	private String presentacion;
+	private String fabricante;
+	private String descripcion;
+	private String tipoEnvase;
+	private String dimensiones;
+	private String pesoTotal;
+	private String existencias;
+	private double precio;
+	private String imgUrl;
+	private boolean enOferta;
+	private double precioOferta;
+	
+	//Constructor con todos los campos -----------------------------------------------------------------------
+	
+	public Productos(Long id, boolean esProducto, String nombre, String marca, String presentacion, String fabricante,
+			String descripcion, String tipoEnvase, String dimensiones, String pesoTotal, String existencias,
+			double precio, String imgUrl, boolean enOferta, double precioOferta) {
+		super();
+		this.id = id;
+		this.esProducto = esProducto;
+		this.nombre = nombre;
+		this.marca = marca;
+		this.presentacion = presentacion;
+		this.fabricante = fabricante;
+		this.descripcion = descripcion;
+		this.tipoEnvase = tipoEnvase;
+		this.dimensiones = dimensiones;
+		this.pesoTotal = pesoTotal;
+		this.existencias = existencias;
+		this.precio = precio;
+		this.imgUrl = imgUrl;
+		this.enOferta = enOferta;
+		this.precioOferta = precioOferta;
+	}
+	
+	//Constructor vacio --------------------------------------------------------------------------------------
+	
+	public Productos() {
+		
+	}
+	
+	//Setter y Getters ---------------------------------------------------------------------------------------
+	
+	public boolean isEsProducto() {
+		return esProducto;
+	}
+
+	public void setEsProducto(boolean esProducto) {
+		this.esProducto = esProducto;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+	public String getPresentacion() {
+		return presentacion;
+	}
+
+	public void setPresentacion(String presentacion) {
+		this.presentacion = presentacion;
+	}
+
+	public String getFabricante() {
+		return fabricante;
+	}
+
+	public void setFabricante(String fabricante) {
+		this.fabricante = fabricante;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public String getTipoEnvase() {
+		return tipoEnvase;
+	}
+
+	public void setTipoEnvase(String tipoEnvase) {
+		this.tipoEnvase = tipoEnvase;
+	}
+
+	public String getDimensiones() {
+		return dimensiones;
+	}
+
+	public void setDimensiones(String dimensiones) {
+		this.dimensiones = dimensiones;
+	}
+
+	public String getPesoTotal() {
+		return pesoTotal;
+	}
+
+	public void setPesoTotal(String pesoTotal) {
+		this.pesoTotal = pesoTotal;
+	}
+
+	public String getExistencias() {
+		return existencias;
+	}
+
+	public void setExistencias(String existencias) {
+		this.existencias = existencias;
+	}
+
+	public double getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+
+	public String getImgUrl() {
+		return imgUrl;
+	}
+
+	public void setImgUrl(String imgUrl) {
+		this.imgUrl = imgUrl;
+	}
+
+	public boolean isEnOferta() {
+		return enOferta;
+	}
+
+	public void setEnOferta(boolean enOferta) {
+		this.enOferta = enOferta;
+	}
+
+	public double getPrecioOferta() {
+		return precioOferta;
+	}
+
+	public void setPrecioOferta(double precioOferta) {
+		this.precioOferta = precioOferta;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	// toString ---------------------------------------------------------------------------------------------
+	
+	@Override
+	public String toString() {
+		return "Productos [id=" + id + ", esProducto=" + esProducto + ", nombre=" + nombre + ", marca=" + marca
+				+ ", presentacion=" + presentacion + ", fabricante=" + fabricante + ", descripcion=" + descripcion
+				+ ", tipoEnvase=" + tipoEnvase + ", dimensiones=" + dimensiones + ", pesoTotal=" + pesoTotal
+				+ ", existencias=" + existencias + ", precio=" + precio + ", imgUrl=" + imgUrl + ", enOferta="
+				+ enOferta + ", precioOferta=" + precioOferta + "]";
+	}
+	
+	
+	
+	
+	
+	
 	
 /*	TODO: Hay que colocar los atributos restantes de la tabla, darle una ultima verificación antes de colocarlos
  *  Se requiere anexar el constructor ya con los parametros BIEN
