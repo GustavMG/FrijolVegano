4ac1399c9cef1c606216bdf5365891f8bb7725c4
GustavMG <magg950506@gmail.com>
2023-07-13 02:41:24 -0600

    Se agrega parametros a la clase Usuarios (entidad) acorde a la tabla usuarios

--- src/main/java/com/FrijolVegano/Entity/Usuarios.java
+++ src/main/java/com/FrijolVegano/Entity/Usuarios.java
@@ -8,22 +8,102 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
 
-//@RestController
-//@RequestMapping (path = "/FrijolVegano/usuarios")
 @Entity
-@Table (name = "producto")
+@Table (name = "usuarios")
 public class Usuarios {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "id", unique = true, nullable = false)
 	private Long id;
+	private boolean admin;
+	private String nombre;
+	private String telefono;
+	private String email;
+	private String contrasenia;
+	private String imagenPerfil;
+	private String direccion;
 	
-/*	TODO: Hay que colocar los atributos restantes de la tabla, darle una ultima verificación antes de colocarlos
- *  Se requiere anexar el constructor ya con los parametros BIEN
- *  Se requeire generar los getters y los setters 
- */
+	//Constructor
+	public Usuarios(Long id, boolean admin, String nombre, String telefono, String email, String contrasenia,
+			String imagenPerfil, String direccion) {
+		this.id = id;
+		this.admin = admin;
+		this.nombre = nombre;
+		this.telefono = telefono;
+		this.email = email;
+		this.contrasenia = contrasenia;
+		this.imagenPerfil = imagenPerfil;
+		this.direccion = direccion;
+	}//Constructor Usuarios
+	
+	public Usuarios() {}//Constructor vacío
+	
+	//Getters y Setters	
+	public Long getId() {
+		return id;
+	}
 
-}
+	public boolean isAdmin() {
+		return admin;
+	}
+
+	public void setAdmin(boolean admin) {
+		this.admin = admin;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getContrasenia() {
+		return contrasenia;
+	}
+
+	public void setContrasenia(String contrasenia) {
+		this.contrasenia = contrasenia;
+	}
+
+	public String getImagenPerfil() {
+		return imagenPerfil;
+	}
+
+	public void setImagenPerfil(String imagenPerfil) {
+		this.imagenPerfil = imagenPerfil;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+	@Override
+	public String toString() {
+		return "Usuarios [id=" + id + ", admin=" + admin + ", nombre=" + nombre + ", telefono=" + telefono + ", email="
+				+ email + ", contrasenia=" + contrasenia + ", imagenPerfil=" + imagenPerfil + ", direccion=" + direccion
+				+ "]";
+	}//toString
+}//Usuarios
