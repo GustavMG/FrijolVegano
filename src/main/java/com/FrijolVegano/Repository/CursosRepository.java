5cc93ec2838d7db2b8ba69f5d8e6541c3e5c81fe
Eliseo de Haro <elidharolpz@gmail.com>
2023-07-13 21:46:20 -0600

    Se agrega la query personalizada

--- src/main/java/com/FrijolVegano/Repository/CursosRepository.java
+++ src/main/java/com/FrijolVegano/Repository/CursosRepository.java
@@ -1,15 +1,17 @@
 package com.FrijolVegano.Repository;
 
-//import java.util.Optional;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.FrijolVegano.Entity.Cursos;
 
-//public interface CursosRepository extends JpaRepository<Cursos, Long> {
+
 @Repository
 public interface CursosRepository extends JpaRepository<Cursos, Long>{
-//	Optional<Cursos> buscarPorCodigoCurso(String codigoCurso);
-	//Creo que el cliente debe ingresar un código interno de curso/producto pq seria dificil buscarlo con nombre(por la longitud)
+	@Query("SELECT u FROM Cursos u WHERE u.titulo = :titulo")
+	Optional<Cursos> buscarPorTitulo(@Param("titulo")String titulo);
 }
